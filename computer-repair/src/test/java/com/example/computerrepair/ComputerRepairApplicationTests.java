cd5cf3814b51e9bc7c3b113a9a663b26a0b1b6b5
YG <9056>
2022-07-02 18:05:33 +0800

    Initial commit

---
+++ computer-repair/src/test/java/com/example/computerrepair/ComputerRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.computerrepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
