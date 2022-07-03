f2f06b3fdcff68cbd48a6cacb1c8c50288f2bb5a
YG <9056>
2022-07-03 09:27:43 +0800

    生成xml文件

--- computer-repair/src/main/java/com/example/computerrepair/ComputerRepairApplication.java
+++ computer-repair/src/main/java/com/example/computerrepair/ComputerRepairApplication.java
@@ -2,8 +2,10 @@ package com.example.computerrepair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ComputerRepairApplication {
 
     public static void main(String[] args) {
